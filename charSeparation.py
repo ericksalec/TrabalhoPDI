481d4abb705edc41c8ae3a7b1114732c45449d58
juan luiz <juanlews@hotmail.com>
2021-10-31 01:41:33 -0300

    comentários traduzidos

--- charSeparation.py
+++ charSeparation.py
@@ -6,68 +6,56 @@ import cv2
 
 
 def removeAntigas():
+	# Cria o diretório caso não exista
 	try:
 		os.makedirs("./imagens/")
 	except FileExistsError:
-		# directory already exists
 		pass
+	#remove todos os arquivos do diretório
 	files = glob.glob('./imagens/*')
 	for f in files:
 		os.remove(f)
 
 def separa(image):
 
+	#Carrega a imagem em tons de cinza e usa o blur para limpar a imagem
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-	# perform edge detection, find contours in the edge map, and sort the
-	# resulting contours from left-to-right
+	# Usa o algoritimo de canny para encontrar bordas e depois contorna-las
 	edged = cv2.Canny(blurred, 30, 150)
 	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	cnts = sort_contours(cnts, method="left-to-right")[0]
 
-	# initialize the list of contour bounding boxes and associated
-	# characters that we'll be OCR'ing
+
 	chars = []
 	removeAntigas()
-	# loop over the contours
+
+	# Percorrer a lista de objetos contornados
 	for c in cnts:
-		# compute the bounding box of the contour
+		# pegando coordenadas, largura e altura.
 		(x, y, w, h) = cv2.boundingRect(c)
-		# filter out bounding boxes, ensuring they are neither too small
-		# nor too large
+
+		# filtrando por tamanho cada objeto, para que não seja nem muito grande nem muito pequeno
 		if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
 			roi = gray[y:y + h, x:x + w]
-			# extract the character and threshold it to make the character
-			# appear as *white* (foreground) on a *black* background, then
-			# grab the width and height of the thresholded image
-			thresh = cv2.threshold(roi, 0, 255,
-								   cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+			# extrai o caractere e aplica o threshold usando o algoritimo de otsu
+			thresh = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+			# pega o tamanho da imagem após o otsu e aplica um rezise proporcional
 			(tH, tW) = thresh.shape
-			# if the width is greater than the height, resize along the
-			# width dimension
 			if tW > tH:
 				thresh = imutils.resize(thresh, width=32)
-			# otherwise, resize along the height
 			else:
 				thresh = imutils.resize(thresh, height=32)
 
-			cv2.imwrite("./imagens/y=" + str(y) + "-h=" + str(h) + "-x=" + str(x) + "-w=" + str(w) + ".jpg", thresh)
-
-			# re-grab the image dimensions (now that its been resized)
-			# and then determine how much we need to pad the width and
-			# height such that our image will be 32x32
+			#escreve as imagens no diretório. o nome do arquivo será composto de informações uteis para quando recuperarmos no futuro
 			(tH, tW) = thresh.shape
 			dX = int(max(0, 32 - tW) / 2.0)
 			dY = int(max(0, 32 - tH) / 2.0)
 
-			# pad the image and force 32x32 dimensions
 			padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
 										left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
 										value=(0, 0, 0))
 			padded = cv2.resize(padded, (32, 32))
-
-			# prepare the padded image for classification via our
-			# handwriting OCR model
-			padded = padded.astype("float32") / 255.0
+			cv2.imwrite("./imagens/y=" + str(y) + "-h=" + str(h) + "-x=" + str(x) + "-w=" + str(w) + ".jpg", padded)
